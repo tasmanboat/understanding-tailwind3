f94d35c83f8b9d4b063f065874823e592a859e9a
noname <you@example.com>
2022-05-12 19:00:26 +0800

    mock server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './core/services/in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -16,10 +20,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     LayoutModule,
     CoreModule,
     // ReaderModule, // lazy load
+
+// #region mock server
+/*
+The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+and returns simulated server responses.
+Remove it when a real server is ready to receive requests.
+*/
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false, passThruUnknownUrl: true, delay: 500 }),
+// #endregion
+
     AppRoutingModule,
   ],
   providers: [],
---
+++ src/app/core/services/in-memory-data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService, ResponseOptions } from 'angular-in-memory-web-api';
+import { FavSubreddit } from "src/app/reader/interfaces/fav-subreddit";
+import { FAV_SUBREDDITS } from './mock-fav-subreddits';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  constructor() { }
+
+// #region API endpoint
+/*
+GET /api/fav-subreddits
+POST /api/fav-subreddits
+GET /api/fav-subreddits/1
+PUT /api/fav-subreddits/1
+DELETE /api/fav-subreddits/1
+*/
+  createDb() {
+    return { 'fav-subreddits': FAV_SUBREDDITS };
+  }
+// #endregion
+
+  genId(records: FavSubreddit[]): number {
+    return records.length > 0 ? Math.max(...records.map(record => record.id))+1 : 11;
+  }
+
+  responseInterceptor(res: ResponseOptions, ri: RequestInfo): ResponseOptions {
+    // console.log((ri as any)?.url);
+    // console.log((ri as any)?.method);
+    // console.log(res.body);
+    // console.log(Array.isArray(res.body));
+    return res;
+  }
+
+}
