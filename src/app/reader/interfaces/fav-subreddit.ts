ce95ecf03e67ab72b04bea047522861195023bc2
noname <you@example.com>
2022-05-18 18:23:20 +0800

    theme

--- src/app/reader/interfaces/fav-subreddit.ts
+++ src/app/reader/interfaces/fav-subreddit.ts
@@ -2,6 +2,7 @@ export interface FavSubreddit {
   id: number;
   name: string;
   isPinned: boolean;
+  isHomepage?: boolean;
   created_at: number;
   updated_at: number;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'suzumecha': '#8F5A3C',
+        'suzumecha-light': '#BC9C8A',
+        'suzumecha-extra-light': '#E9DED8',
+        'fountainblue': '#4AA5B8',
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
