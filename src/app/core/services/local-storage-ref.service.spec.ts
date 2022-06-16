e807feeb14f9c6c821d5191e58c7a21ab30ebce9
noname <you@example.com>
2022-06-16 18:30:38 +0800

    core service tests

--- src/app/core/services/local-storage-ref.service.spec.ts
+++ src/app/core/services/local-storage-ref.service.spec.ts
@@ -2,7 +2,52 @@ import { TestBed } from '@angular/core/testing';
 
 import { LocalStorageRefService } from './local-storage-ref.service';
 
+describe('LocalStorageRefService without Angular testing support', () => {
+  let service: LocalStorageRefService;
+
+  it('should be created', () => {
+    service = new LocalStorageRefService();
+    expect(service).toBeDefined();
+  });
+
+  it('localStorage property should return localStorage', () => {
+    service = new LocalStorageRefService();
+    let ls: Storage = localStorage;
+    expect(service.localStorage).toBe(ls);
+  });
+
+  it('localStorage property should return singleton localStorage', () => {
+    service = new LocalStorageRefService();
+    expect(service.localStorage).toBe(localStorage);
+  });
+});
+
 describe('LocalStorageRefService', () => {
+  let service: LocalStorageRefService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [LocalStorageRefService]
+    });
+    service = TestBed.inject(LocalStorageRefService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('localStorage property should return localStorage', () => {
+    let ls: Storage = localStorage;
+    expect(service.localStorage).toBe(ls);
+  });
+
+  it('localStorage property should return singleton localStorage', () => {
+    expect(service.localStorage).toBe(localStorage);
+  });
+});
+
+// CLI-generated tests
+xdescribe('LocalStorageRefService (CLI-generated tests)', () => {
   let service: LocalStorageRefService;
 
   beforeEach(() => {
--- src/app/core/services/persistent-storage.service.spec.ts
+++ src/app/core/services/persistent-storage.service.spec.ts
@@ -1,8 +1,87 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PersistentStorageService } from './persistent-storage.service';
+import { LocalStorageRefService } from './local-storage-ref.service';
+
+describe('PersistentStorageService without Angular testing support', () => {
+  let service: PersistentStorageService;
+
+  it('should be created', () => {
+    service = new PersistentStorageService(new LocalStorageRefService());
+    expect(service).toBeDefined();
+  });
+
+  it('#getItemAsync should return real value from the real service', () => {
+    service = new PersistentStorageService(new LocalStorageRefService());
+    expect(service.getItemAsync('fav-subreddits')).toBeInstanceOf(Promise);
+  });
+});
+
+describe('PersistentStorageService without Angular testing support, with spy', () => {
+  let service: PersistentStorageService;
+
+  it('should be created', () => {
+    service = new PersistentStorageService(new LocalStorageRefService());
+    expect(service).toBeDefined();
+  });
+
+  it('#getItemAsync should use stubbed value from a spy', () => {
+    const _singletonLocalStorage: Storage = localStorage;
+    const localStorageRefServiceSpy = jasmine.createSpyObj('LocalStorageRefService', {}, { localStorage: _singletonLocalStorage });
+
+    expect(localStorageRefServiceSpy.localStorage).toBeInstanceOf(Storage);
+    expect(localStorageRefServiceSpy.localStorage).toEqual(localStorage);
+
+    service = new PersistentStorageService(localStorageRefServiceSpy);
+    expect(service.getItemAsync('fav-subreddits')).toBeInstanceOf(Promise);
+  });
+});
 
 describe('PersistentStorageService', () => {
+  let service: PersistentStorageService;
+  let localStorageRefServiceSpy: jasmine.SpyObj<LocalStorageRefService>;
+
+  beforeEach(() => {
+    const _singletonLocalStorage: Storage = localStorage;
+    const spy = jasmine.createSpyObj('LocalStorageRefService', {}, { localStorage: _singletonLocalStorage });
+
+    TestBed.configureTestingModule({
+      // Provide both the service-to-test and its (spy) dependency
+      providers: [
+        PersistentStorageService,
+        { provide: LocalStorageRefService, useValue: spy }
+      ]
+    });
+
+    // Inject both the service-to-test and its (spy) dependency
+    service = TestBed.inject(PersistentStorageService);
+    localStorageRefServiceSpy = TestBed.inject(LocalStorageRefService) as jasmine.SpyObj<LocalStorageRefService>;
+  });
+
+  it('should be created', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('localStorage property should use stubbed value from a spy', () => {
+    const ls: Storage = localStorage;
+    expect(localStorageRefServiceSpy.localStorage).toBeInstanceOf(Storage);
+    expect(localStorageRefServiceSpy.localStorage).toEqual(ls);
+  });
+
+  it('#getItemAsync should use stubbed value from a spy', () => {
+    const ls: Storage = localStorage;
+
+    expect(localStorageRefServiceSpy.localStorage).toBeInstanceOf(Storage);
+    expect(localStorageRefServiceSpy.localStorage).toEqual(ls);
+
+    expect(service.getItemAsync('fav-subreddits'))
+      .withContext('service used stub value')
+      .toBeInstanceOf(Promise);
+  });
+});
+
+// CLI-generated tests
+xdescribe('PersistentStorageService (CLI-generated tests)', () => {
   let service: PersistentStorageService;
 
   beforeEach(() => {
